46827686c9eae8102fdb234ee9dac35856004b4c
Sky <skyblue3742@gmail.com>
2024-04-11 23:32:30 +0800

    feat: update dashboard layout

---
+++ src/routes/(dashboard)/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Navbar from '$lib/components/layout/navbar/Navbar.svelte';
+	import Sidenav from '$lib/components/layout/sidebar/Sidebar.svelte';
+</script>
+
+<div class="bg-gray-50 antialiased dark:bg-gray-900">
+	<Navbar />
+
+	<Sidenav />
+
+	<main class="h-auto p-4 pt-20 md:ml-64">
+		<slot />
+	</main>
+</div>
---
+++ src/routes/(dashboard)/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { A, Heading, P } from 'flowbite-svelte';
+</script>
+
+<Heading tag="h1">Welcome to SvelteKit</Heading>
+<P>Visit <A href="https://kit.svelte.dev">kit.svelte.dev</A> to read the documentation</P>
